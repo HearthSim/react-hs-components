1be32a6410051d7be7c136cc9e9f78243da3d654
Jerome Leclanche <jerome@leclan.ch>
2018-07-12 12:38:05 +0300

    Add simple tests for Card component

---
+++ src/components/__tests__/Card.spec.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import TestRenderer from "react-test-renderer";
+import Card from "../Card";
+
+describe("Card", () => {
+	test("renders correctly", () => {
+		const component = TestRenderer.create(<Card id="EX1_001" />);
+		const tree = component.toJSON();
+		expect(tree).toMatchObject(
+			TestRenderer.create(
+				<img src="https://art.hearthstonejson.com/v1/render/latest/enUS/256x/EX1_001.png" />,
+			).toJSON(),
+		);
+	});
+
+	test("resolution prop works", () => {
+		const component = TestRenderer.create(
+			<Card id="EX1_001" resolution={512} />,
+		);
+		const tree = component.toJSON();
+		expect(tree).toMatchObject(
+			TestRenderer.create(
+				<img src="https://art.hearthstonejson.com/v1/render/latest/enUS/512x/EX1_001.png" />,
+			).toJSON(),
+		);
+	});
+});
