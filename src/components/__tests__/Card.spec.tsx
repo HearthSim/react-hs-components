e5a8a16f32578ac6495f00cc33f9cb31ac7f5553
Édipo da Silva Souza <edipo2s@gmail.com>
2019-12-19 17:07:02 -0300

    Fix tests

--- src/components/__tests__/Card.spec.tsx
+++ src/components/__tests__/Card.spec.tsx
@@ -6,7 +6,7 @@ describe("Card", () => {
 	test("renders correctly", () => {
 		const component = TestRenderer.create(<Card id="EX1_001" />);
 		const tree = component.toJSON();
-		expect(tree).toMatchObject(
+		expect(tree).toEqual(
 			TestRenderer.create(
 				<img src="https://art.hearthstonejson.com/v1/render/latest/enUS/256x/EX1_001.png" />,
 			).toJSON(),
@@ -18,7 +18,7 @@ describe("Card", () => {
 			<Card id="EX1_001" resolution={512} />,
 		);
 		const tree = component.toJSON();
-		expect(tree).toMatchObject(
+		expect(tree).toEqual(
 			TestRenderer.create(
 				<img src="https://art.hearthstonejson.com/v1/render/latest/enUS/512x/EX1_001.png" />,
 			).toJSON(),
@@ -28,7 +28,7 @@ describe("Card", () => {
 	test("locale prop works", () => {
 		const component = TestRenderer.create(<Card id="EX1_001" locale="frFR" />);
 		const tree = component.toJSON();
-		expect(tree).toMatchObject(
+		expect(tree).toEqual(
 			TestRenderer.create(
 				<img src="https://art.hearthstonejson.com/v1/render/latest/frFR/256x/EX1_001.png" />,
 			).toJSON(),
