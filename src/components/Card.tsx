588d3078f637e0fab02fbf5d89108e9f9156b44c
Benedict Etzel <benedict@hearthsim.net>
2020-03-27 12:05:22 +0100

    Add Battlegrounds support to Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -26,6 +26,8 @@ export interface CardProps {
 	resolution?: 256 | 512;
 	className?: string;
 	placeholder?: string;
+	battlegrounds?: boolean;
+	triple?: boolean;
 	style?: React.CSSProperties;
 }
 
@@ -52,9 +54,11 @@ export default class Card extends React.Component<CardProps, CardState> {
 	}
 
 	render(): JSX.Element | any | false {
-		const artUrl = `https://art.hearthstonejson.com/v1/render/latest/${
+		const prefix = this.props.battlegrounds ? "bgs" : "render";
+		const cardId = this.props.id + (this.props.triple ? "_triple" : "");
+		const artUrl = `https://art.hearthstonejson.com/v1/${prefix}/latest/${
 			this.props.locale || "enUS"
-		}/${this.props.resolution || 256}x/${this.props.id}.png`;
+		}/${this.props.resolution || 256}x/${cardId}.png`;
 		if (!this.props.placeholder) {
 			return (
 				<img
--- src/components/__tests__/Card.spec.tsx
+++ src/components/__tests__/Card.spec.tsx
@@ -3,7 +3,7 @@ import TestRenderer from "react-test-renderer";
 import Card from "../Card";
 
 describe("Card", () => {
-	test("renders correctly", () => {
+	it("renders correctly", () => {
 		const component = TestRenderer.create(<Card id="EX1_001" />);
 		const tree = component.toJSON();
 		expect(tree).toEqual(
@@ -13,7 +13,7 @@ describe("Card", () => {
 		);
 	});
 
-	test("resolution prop works", () => {
+	it("can render at different resolutions", () => {
 		const component = TestRenderer.create(
 			<Card id="EX1_001" resolution={512} />,
 		);
@@ -25,12 +25,34 @@ describe("Card", () => {
 		);
 	});
 
-	test("locale prop works", () => {
+	it("can render localized cards", () => {
 		const component = TestRenderer.create(<Card id="EX1_001" locale="frFR" />);
 		const tree = component.toJSON();
 		expect(tree).toEqual(
 			TestRenderer.create(
 				<img src="https://art.hearthstonejson.com/v1/render/latest/frFR/256x/EX1_001.png" />,
+			).toJSON(),
+		);
+	});
+
+	it("renders a battlegrounds card", () => {
+		const component = TestRenderer.create(<Card id="EX1_506" battlegrounds />);
+		const tree = component.toJSON();
+		expect(tree).toEqual(
+			TestRenderer.create(
+				<img src="https://art.hearthstonejson.com/v1/bgs/latest/enUS/256x/EX1_506.png" />,
+			).toJSON(),
+		);
+	});
+
+	it("renders a tripled battlegrounds card", () => {
+		const component = TestRenderer.create(
+			<Card id="TB_BaconUps_003" battlegrounds triple />,
+		);
+		const tree = component.toJSON();
+		expect(tree).toEqual(
+			TestRenderer.create(
+				<img src="https://art.hearthstonejson.com/v1/bgs/latest/enUS/256x/TB_BaconUps_003_triple.png" />,
 			).toJSON(),
 		);
 	});
